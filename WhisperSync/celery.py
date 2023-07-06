b45c57362d980855f3fd8c4c80e7fce77163f2d1
hammadtariq838 <24100267@lums.edu.pk>
2023-07-07 00:29:52 +0500

    MVP done but a strong error handling has yet to be implemented

--- WhisperSync/celery.py
+++ WhisperSync/celery.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, unicode_literals
-
 import os
 
 from celery import Celery
@@ -11,3 +9,8 @@ app = Celery("whispersync")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- subtitles/models.py
+++ subtitles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # get_user_model
 from django.contrib.auth import get_user_model
+from .utils import convert_seconds_to_hms
 
 User = get_user_model()
 
@@ -11,9 +12,19 @@ class Subtitle(models.Model):
     title = models.CharField(max_length=200)
     length = models.IntegerField()  # in seconds
     thumbnail_url = models.URLField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, default='pending')
     cost = models.IntegerField()
+    is_paid = models.BooleanField(default=False)
     error = models.CharField(max_length=200, null=True, blank=True)
     language = models.CharField(max_length=200, default='en')
     file_path = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.status}'
+
+    def get_display_length(self):
+        return convert_seconds_to_hms(self.length)
+
+    def get_display_cost(self):
+        return f'{self.cost / 100}'
